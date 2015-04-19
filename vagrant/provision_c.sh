723612305d1ed1118474d86f893c7f04e67320ef
stevengonsalvez <steven.gonsalvez@gmail.com>
2015-04-19 22:08:34 +0100

    Steve : changes to the provisioning scripts

--- vagrant/provision_c.sh
+++ vagrant/provision_c.sh
@@ -1,12 +1,13 @@
 
 #yum installs
 sudo yum install dos2unix
-yum install -y java-1.7.0-openjdk.x86_64
-yum install -y mlocate
-yum install -y telnet
-yum install -y screen
-yum install -y vim
-yum install -y git
+sudo yum install -y java-1.7.0-openjdk.x86_64
+sudo yum install -y mlocate
+sudo yum install -y telnet
+sudo yum install -y screen
+sudo yum install -y vim
+sudo yum install -y git
+sudo yum update -y
 
 ##cloning the repository for various files
 git clone https://github.com/stevengonsalvez/containerisation.git
@@ -14,4 +15,19 @@ git clone https://github.com/stevengonsalvez/containerisation.git
 ##switching off iptables
 sudo chkconfig iptables off
 sudo /etc/init.d/iptables stop
+sudo service firewalld stop
+
+###alternatively we could just add firewall rules
+##firewall-cmd --zone=public --add-port=8080/tcp --permanent
+##firewall-cmd --zone=public --add-service=http --permanent
+##firewall-cmd --reload
+
+## adding jenkins repository
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins
+
+sudo chkconfig jenkins on
+sudo service jenkins start/stop/restart
+
 
--- vagrant/provision_i.sh
+++ vagrant/provision_i.sh
@@ -5,12 +5,13 @@ mule_ce_location="https://repository-master.mulesoft.org/nexus/content/repositor
 
 #yum installs
 sudo yum install dos2unix
-yum install -y java-1.7.0-openjdk.x86_64
-yum install -y mlocate
-yum install -y telnet
-yum install -y screen
-yum install -y vim
-yum install -y git
+sudo yum install -y java-1.7.0-openjdk.x86_64
+sudo yum install -y mlocate
+sudo yum install -y telnet
+sudo yum install -y screen
+sudo yum install -y vim
+sudo yum install -y git
+sudo yum update -y
 
 ##cloning the repository for various files
 git clone https://github.com/stevengonsalvez/containerisation.git
@@ -18,6 +19,12 @@ git clone https://github.com/stevengonsalvez/containerisation.git
 ##switching off iptables
 sudo chkconfig iptables off
 sudo /etc/init.d/iptables stop
+sudo service firewalld stop
+
+###alternatively we could just add firewall rules
+##firewall-cmd --zone=public --add-port=8080/tcp --permanent
+##firewall-cmd --zone=public --add-service=http --permanent
+##firewall-cmd --reload
 
 ##retrieving package of mule from the internet
 wget ${mule_ce_location}
