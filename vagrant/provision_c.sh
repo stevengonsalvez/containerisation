06aa0ba42ea409e4165e58fed7f32b49f03dd825
steven gonsalvez <steven.gonsalvez@gmail.com>
2015-09-13 09:21:02 +0100

    steve: changes for nexus

--- vagrant/provision_c.sh
+++ vagrant/provision_c.sh
@@ -3,42 +3,6 @@ nexus_ce_location= "http://download.sonatype.com/nexus/oss/nexus-2.11.0-bundle.t
 user_to_run_nexus="nexus"
 directory_for_nexus_store="/srv/nexus/main-repo"
 
-
-
-#yum installs
-sudo yum install -y dos2unix
-sudo yum install -y java-1.7.0-openjdk.x86_64
-sudo yum install -y mlocate
-sudo yum install -y telnet
-sudo yum install -y screen
-sudo yum install -y vim
-sudo yum install -y git
-sudo yum update -y
-
-##cloning the repository for various files
-git clone https://github.com/stevengonsalvez/containerisation.git
-
-##switching off iptables
-sudo chkconfig iptables off
-sudo /etc/init.d/iptables stop
-sudo service firewalld stop
-
-###alternatively we could just add firewall rules
-##firewall-cmd --zone=public --add-port=8080/tcp --permanent
-##firewall-cmd --zone=public --add-service=http --permanent
-##firewall-cmd --reload
-
-## adding jenkins repository - jenkins runs on port 8080
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install -y jenkins
-
-sudo chkconfig jenkins on
-sudo service jenkins start/stop/restart
-
-
-nexus_install
-
 #### Installation of nexus function
 nexus_install(){
 
@@ -84,3 +48,38 @@ sudo service nexus start
 
 
 }
+
+#yum installs
+sudo yum install -y dos2unix
+sudo yum install -y java-1.7.0-openjdk.x86_64
+sudo yum install -y mlocate
+sudo yum install -y telnet
+sudo yum install -y screen
+sudo yum install -y vim
+sudo yum install -y git
+sudo yum update -y
+
+##cloning the repository for various files
+git clone https://github.com/stevengonsalvez/containerisation.git
+
+##switching off iptables
+sudo chkconfig iptables off
+sudo /etc/init.d/iptables stop
+sudo service firewalld stop
+
+###alternatively we could just add firewall rules
+##firewall-cmd --zone=public --add-port=8080/tcp --permanent
+##firewall-cmd --zone=public --add-service=http --permanent
+##firewall-cmd --reload
+
+## adding jenkins repository - jenkins runs on port 8080
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install -y jenkins
+
+sudo chkconfig jenkins on
+sudo service jenkins start/stop/restart
+
+
+nexus_install
+
